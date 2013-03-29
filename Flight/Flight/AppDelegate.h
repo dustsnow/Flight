82102082db7017ecb3994307b5336b1ea8374aa8
peng hou <dustsnow@gmail.com>
2013-03-29 11:21:31 -0400

    Create  Components of the APP

---
+++ Flight/Flight/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flight
+//
+//  Created by peng hou on 3/29/13.
+//  Copyright (c) 2013 Peng Hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
